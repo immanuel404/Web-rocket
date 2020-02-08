0c8b60ecbc56b93f39a38e9c3354464e26c7533e
Immanuel <57101108+immanuel404@users.noreply.github.com>
2020-02-08 10:15:28 +0200

    Add files via upload

---
+++ java/scripts.js
@@ -0,0 +1,30 @@
+var sayMyName= function(name){
+	alert("My name is "+ name);
+}
+var doCoolStuff= function(){
+	document.getElementById('cool').className = 'cool red';
+}
+// var car = {
+// 	make: 'VW';
+// 	type: 'Polo',
+// 	color: 'Blue',
+// 	isTurnedOn: false,
+// 	numberOfWHheels: 4,
+// 	}
+// 	seats: ['seat 1', 'seat 2', 'seat 3', 'seat 4'],
+// 	turnOn: function(){
+// 		this.isTurnedOn = True;
+// 	},
+// 	fly: funtion(){
+// 	alert('fly');
+// 	},
+// 	switchCar: function (isOn){
+// 		console.log('turn car '+isOn);
+// 		if (isOn== true){
+// 		this.osTurnedOn = true;
+// 		} else{
+// 		this.isTurnedOn = false;
+// 		}	
+// 	}
+
+// };
